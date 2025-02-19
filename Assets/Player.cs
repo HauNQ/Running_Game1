0ed3c913697112dcb74b9c28593d90c7a502e174
Nguyen Quang Hau <quanghau6622@gmail.com>
2025-02-20 03:46:56 +0700

    When an user type A Key, the game will be started
    add Start Button

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,9 +4,9 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public bool runBegun;
 
     public Rigidbody2D rb;
-    public Transform Transform;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,10 +17,27 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+        if (runBegun)
+        {
+            Debug.Log("Running.....");
+            rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+        }
+
+        checkInput();
+    }
+
+    private void checkInput()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Type A Key");
+            runBegun = true;
+        }
+
         if (Input.GetButtonDown("Jump"))
         {
-            Debug.Log("I am jumping "+jumpForce);
+            Debug.Log("I am jumping " + jumpForce);
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
     }
