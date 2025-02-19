cd903206e91ea3e217ced43a7ce8ef8404c673f5
Nguyen Quang Hau <quanghau6622@gmail.com>
2025-02-20 04:23:26 +0700

    The player will know where is the ground, the player can be jumped only when it  is  on the ground.
    Add Ground Object

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Move Infor")]
     public float moveSpeed;
     public float jumpForce;
-    public bool runBegun;
+    private bool runBegun;
 
     public Rigidbody2D rb;
+
+    [Header("Collission Infor")]
+    public float groundCheckDistance;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,7 +30,13 @@ public class Player : MonoBehaviour
             rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
         }
 
+        CheckCollission();
         checkInput();
+    }
+
+    private void CheckCollission()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundMask);
     }
 
     private void checkInput()
@@ -35,10 +48,16 @@ public class Player : MonoBehaviour
             runBegun = true;
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             Debug.Log("I am jumping " + jumpForce);
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y-groundCheckDistance));
     }
 }
